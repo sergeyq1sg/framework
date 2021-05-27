9cde97f5675f78088b9b7a2e4dc2a61515fb161e
sergey123 <popkokaratel2005@gmail.com>
2021-05-27 13:37:37 -0400

    extended framework

--- index.js
+++ index.js
@@ -10,3 +10,27 @@ try {
 for (let index = 0; index < "dskj".length; index++) {
   console.log(index);
 }
+export default function createFramework() {
+  console.log("sosi huy");
+}
+try {
+  throw new Error("dflk");
+} catch (error) {
+  console.log(error);
+}
+
+for (let index = 0; index < "dskj".length; index++) {
+  console.log(index);
+}
+export default function createFramework() {
+  console.log("sosi huy");
+}
+try {
+  throw new Error("dflk");
+} catch (error) {
+  console.log(error);
+}
+
+for (let index = 0; index < "dskj".length; index++) {
+  console.log(index);
+}
